99224cb4ef5781303db03da3a450019647b5b836
Toby Penk <tobypenk@gmail.com>
2021-03-11 11:39:39 -0500

    comment db_functions library

--- php/libraries/db_functions.php
+++ php/libraries/db_functions.php
@@ -1,6 +1,14 @@
 <?php
 
 	function open_connection() {
+		
+		/*
+			creates a database connection and places it in the global namespace
+			must be called before any database functions (queries, inserts, etc) are called
+			
+			parameters: none
+			returns: none (places $conn in the global namespace)
+		*/
 
 		global $conn;
 
@@ -14,6 +22,14 @@
 	}
 
 	function close_connection() {
+		
+		/*
+			destroys the connection created by open_connection
+			must be called to close any database script or the lingering connections will exceed the limit
+			
+			parameters: none
+			returns: none (removes $conn from the global namespace)
+		*/
 
 		global $conn;
 
@@ -22,6 +38,20 @@
 	}
 
 	function execute_query($query_string) {
+		
+		/*
+			executes a query and returns an array with the results
+			
+			parameters:
+				query_string: a string with an executable query
+				
+			returns:
+				result (array)
+				
+			note - open_connection() must be called; otherwise, this function will not find the right connection object
+				in the global namespace and will create a nonworking one
+		*/
+		
 		global $conn;
 		
 	    $q = mysqli_query($conn,$query_string);
@@ -36,12 +66,41 @@
 
 	function execute_insertion($query_string) {
 
+		/*
+			executes an insertion into the database
+			
+			parameters:
+				query_string: a string with an executable insertion query
+				
+			returns:
+				none
+				
+			note - open_connection() must be called; otherwise, this function will not find the right connection object
+				in the global namespace and will create a nonworking one
+		*/
+		
         global $conn;
 
 	    $q = mysqli_query($conn,$query_string);
 	}
 	
 	function execute_removal($query_string) {
+		
+		/*
+			executes a deletion query
+			
+			parameters:
+				query_string: a string with an executable deletion query
+				
+			returns:
+				result (array)
+				
+			note - open_connection() must be called; otherwise, this function will not find the right connection object
+				in the global namespace and will create a nonworking one
+				
+			note - this is identical to execute_insertion(); a future version may refactor this to a single 
+				execute_nonreturning_query function
+		*/
 
         global $conn;
 
@@ -49,6 +108,20 @@
 	}
 
 	function execute_insertion_and_return_insert_id($query_string,$table) {
+		
+		/*
+			executes an insertion and returns the id of the inserted, blank row
+			
+			parameters:
+				query_string: a string with an executable query
+				table: the table into which the item is being inserted
+				
+			returns:
+				last_insert_id (int as string) representing the last insert id
+				
+			note - open_connection() must be called; otherwise, this function will not find the right connection object
+				in the global namespace and will create a nonworking one
+		*/
 
 		global $conn;
 
